8a04854a100618357188d523f469622a44e18d15
Marlon Monzon <martianworm17@gmail.com>
2018-12-20 23:13:23 +0800

    Initial commit

---
+++ src/main/java/com/tuwaisu/tzuyu/TzuyuApplication.java
@@ -0,0 +1,14 @@
+package com.tuwaisu.tzuyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TzuyuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TzuyuApplication.class, args);
+	}
+
+}
+
